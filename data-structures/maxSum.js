bbef2010351871905213830f6cdbeeaf92211225
Eben Palazzolo <olozzalap@gmail.com>
2019-01-28 12:03:47 -0800

    Improved maxSum.js to linear time with Kadanes algorithm

--- data-structures/maxSum.js
+++ data-structures/maxSum.js
@@ -1,27 +1,40 @@
 // You are given an array containing positive and negative integers. Write an algorithm which will find the largest sum in a continuous sequence.
 
 function maxSum(arr) {
-	let sums = [];
 	let ticks = 1;
-	for (let i = 0; i < arr.length; i++) {
-		let newSum = 0;
-		for (let j = i; j < arr.length; j++) {
-			// console.log(i, j);
-			ticks++;
-			newSum += arr[j];
-			sums.push({
-				sum: newSum,
-				start: i,
-				end: j
-			});
-		}
-	}
-	sums.sort((a, b) => { return b.sum - a.sum});
-	// console.log(sums);
+	let currentMax = 0;
+	let currentStartIndex = 0;
+    let max = {
+    	sum: 0,
+    	start: 0,
+    	end: 0
+    };
+    for (let i = 0; i < arr.length; i++) {
+    	ticks++;
+    	// console.log(currentMax + arr[i]);
+    	// Either subArray up till i is above zero (so adds to a total sum) or it can be discarded if negative
+    	if (currentMax + arr[i] > 0) {
+    		currentMax = currentMax + arr[i];
+    	}
+    	else {
+    		currentMax = 0;
+    		currentStartIndex = i + 1;
+    	}
+    	
+    	if (currentMax > max.sum) {
+    		max = {
+    			sum: currentMax,
+    			start: currentStartIndex,
+    			end: i
+    		}	
+    	}
+    }
+
 	console.log("input length: " + arr.length + " & total ticks: " + ticks + " for a ticks-to-input ratio of: " + (ticks / arr.length));
-	return sums[0];
+	return max;
 }
 
+console.log(maxSum([8, -5, -3, 7]));
 console.log(maxSum([8, -8]));
 console.log(maxSum([9, -4, 6, 3]));
 console.log(maxSum([4,6,-3,5,-2,1]));

0a8d4fd9ec1dc86411ea9b164d87c81e4407247a
Eben Palazzolo <olozzalap@gmail.com>
2019-01-29 20:16:37 -0800

    LinkedList added insertBefore, insertAfter & insertAt. Fixed bug with inserting first node via insertLast()

--- linkedLists/linkedList.js
+++ linkedLists/linkedList.js
@@ -5,7 +5,7 @@ class _Node {
     }
 }
 
-class LinkedLink {
+class LinkedList {
 	constructor() {
 		this.head = null;
 	}
@@ -15,16 +15,69 @@ class LinkedLink {
 	}
 	insertLast(item) {
 		// list is empty
+		console.log(this.head);
 		if (this.head === null) {
-			this.insertFirst(item);
+			return this.insertFirst(item);
 		}
-		newLastNode = new _Node(item, null);
-		nodeInList = this.head;
+		let newLastNode = new _Node(item, null);
+		let nodeInList = this.head;
 		while (nodeInList.next !== null) {
-			console.log(nodeInList);
+			// console.log(nodeInList);
 			nodeInList = nodeInList.next;
 		}
 		nodeInList.next = newLastNode;
+		console.log(nodeInList, newLastNode);
+	}
+	insertBefore(nodeKey, newItem) {
+		if (!this.head) {
+			return null;
+		}
+
+		let currNode = this.head;
+		let prevNode = this.head;
+		while(currNode.item !== nodeKey) {
+			if (currNode.next === null) {
+				return null;
+			}
+			else {
+				prevNode = currNode;
+				currNode = currNode.next;
+			}
+		}
+		prevNode.next = new _Node(newItem, currNode);
+	}
+	insertAfter(nodeKey, newItem) {
+		if (!this.head) {
+			return null;
+		}
+
+		let currNode = this.head;
+		let prevNode = this.head;
+		while(prevNode.item !== nodeKey) {
+			if (prevNode.next === null) {
+				return null;
+			}
+			else {
+				prevNode = currNode;
+				currNode = currNode.next;
+			}
+		}
+		prevNode.next = new _Node(newItem, currNode);
+	}
+	insertAt(index, item) {
+		if (!this.head) {
+			return null;
+		}
+		let currNode = this.head;
+		let prevNode = this.head;
+		for (let i = 0; i < index; i++) {
+			if (currNode.next === null) {
+				return null;
+			}
+			prevNode = currNode;
+			currNode = currNode.next;
+		}
+		prevNode.next = new _Node(item, currNode);
 	}
 	find(item) {
 		let currNode = this.head;
@@ -63,3 +116,19 @@ class LinkedLink {
 		prevNode.next = currNode.next;
 	}
 }
+
+
+function main() {
+	SLL = new LinkedList()
+
+	SLL.insertLast("Apollo");
+	SLL.insertLast("Boomer");
+	SLL.insertLast("Helo");
+	SLL.insertLast("Husker");
+	SLL.insertLast("Starbuck");
+
+	SLL.insertFirst("Tauhida");
+	SLL.remove("squirrel");
+	console.log(JSON.stringify(SLL));
+}
+main();

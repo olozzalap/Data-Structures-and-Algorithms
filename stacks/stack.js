adf54fe92c40f68263b40f5b107c19a26f48abcd
Eben Palazzolo <olozzalap@gmail.com>
2019-01-31 18:50:02 -0800

    Implemented exhaustive sortStackAsc()

--- stacks/stack.js
+++ stacks/stack.js
@@ -29,6 +29,15 @@ class Stack {
 	peek() {
 		return this.top;
 	}
+	size() {
+		let node = this.top
+		let count = 0
+		while(node) {
+			count++;
+			node = node.next;
+		}
+		return count;
+	}
 }
 
 let stack = new Stack();
@@ -81,7 +90,6 @@ function checkMatchingBrackets(str) {
 	}
 	let chars = str.split("");
 	let bracketsStack = new Stack();
-	let quotesStack = new Stack();
 	let inSingleQuote = false;
 	let inDoubleQuote = false;
 	for (let i = 0; i < chars.length; i++) {
@@ -130,7 +138,6 @@ function checkMatchingBrackets(str) {
 	}
 	return true;
 }
-
 // console.log(checkMatchingBrackets("2133()(adnad(awdad8ad(adand)adnwad)ankawd)")); // true 
 // console.log(checkMatchingBrackets("61(()")); // { index: 2, char: '(' }
 // console.log(checkMatchingBrackets("61)(()")); // { index: 2, char: ')' }
@@ -141,3 +148,58 @@ function checkMatchingBrackets(str) {
 // console.log(checkMatchingBrackets("61{(('}[{}[(0(0)(0()()()}}{}{)(][][)(}{)(][][')[ohno]}")); // { index: 3, char: '(' }
 // console.log(checkMatchingBrackets('[{a: "}{)(][}{)(}{{}{HOTFRESHCODE", b: ({1:[a, b, c]})}]')); // true
 // console.log(checkMatchingBrackets('[{a: }"{)(][}{)(}{{}{HOTFRESHCODE", b: ({1:[a, b, c]})}]')); // { index: 0, char: '[' }
+
+// Sorts a given stack from smallest to largest by iterating through stack.size() times to find the next largest element and pushes that into a new sortedStack, accounts for duplicate values
+function sortStackAsc(stack) {
+	let unsortedStack = Object.assign( Object.create( Object.getPrototypeOf(stack)), stack);
+	let sortedStack = new Stack();
+	let largest = null;
+	let nextLargest = null;
+	let size = stack.size();
+	let dupCount = 0;
+	let prevDupCount = 1;
+	for (let i = 0; i < size; i++) {
+		console.log(i);
+		while (unsortedStack.peek() !== null) {
+			let node = unsortedStack.pop();
+			if (largest === node) {
+				dupCount++;
+				console.log(node, dupCount, prevDupCount);
+			}
+			// If we have atleast two duplicates and this is one more duplicate than the last one we found
+			if (dupCount >= 2 && prevDupCount + 1 === dupCount) {
+				nextLargest = node;
+				prevDupCount = dupCount;
+				break;
+			}
+			// if we have a largest (not the first iteration), that largest is larger than this node and the node is larger than the nextLargest OR (first iteration) if we don't have a largest value yet (null) than assign if node is greater than nextLargest
+			else if ((largest && largest > node && node > nextLargest) || (!largest && node > nextLargest)) {
+				nextLargest = node;
+			}
+		}
+		sortedStack.push(nextLargest);
+		largest = nextLargest;
+		unsortedStack = Object.assign( Object.create( Object.getPrototypeOf(stack)), stack);
+		nextLargest = null;
+		// If we didn't find atleast two duplicates (their will always be one) than set prevDupCount back to 1
+		if (dupCount < 2) {
+			prevDupCount = 1;
+		}
+		dupCount = 0;
+	}
+	return sortedStack;
+}
+let unsortedStack = new Stack();
+unsortedStack.push(7);
+unsortedStack.push(1);
+unsortedStack.push(9);
+unsortedStack.push(3);
+unsortedStack.push(4);
+unsortedStack.push(1);
+unsortedStack.push(15);
+unsortedStack.push(8);
+unsortedStack.push(7);
+unsortedStack.push(2);
+unsortedStack.push(10);
+unsortedStack.push(5);
+console.log(sortStackAsc(unsortedStack)); // 1, 1, 2, 3, 4, 5, 7, 7, 8, 9, 10, 15
